0bfe56576b1def90b610971ebe99e15d7882e9a8
wxy <2357191256@qq.com>
2022-03-23 09:48:03 +0800

    从gitee移动到github

--- 技术文档/FTP代理/startFTP.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-jarpath=/opt/sgg/engine/ftp
-nohup java -jar $jarpath/DTEF*.jar $jarpath/$1 > /dev/null  2>&1 &
-echo $! > $jarpath/$1.pid
--- 技术文档/FTP代理/stopFTP.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-CONFIG=/opt/sgg/engine/ftp/$1
-PID=$(cat $CONFIG.pid)
-# 安全退出
-kill -15 $PID
-rm -f $CONFIG
-rm -f $CONFIG.pid
